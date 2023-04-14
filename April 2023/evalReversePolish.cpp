b3834a02c4bd9c1354ecbd01820131dfa649f91e
croja006 <crojas99@csu.fullerton.edu>
2023-04-13 23:08:24 -0700

    Not the fastest but able to produce pretty good memory performance.

--- April 2023/evalReversePolish.cpp
+++ April 2023/evalReversePolish.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        
+        stack<int> myStack;
+        int leftString;
+        int rightString;
+        for(int i = 0; i < tokens.size(); ++i){
+            if((tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" ) && !myStack.empty()){
+                rightString = myStack.top();
+                myStack.pop();
+                leftString = myStack.top();
+                myStack.pop();
+                if(tokens[i] == "+"){
+                     myStack.push(leftString + rightString);
+                }
+                if(tokens[i] == "-"){
+                    myStack.push(leftString - rightString);
+                }
+                if(tokens[i] == "*"){
+                    myStack.push(leftString * rightString);
+                }
+                if(tokens[i] == "/"){
+                    myStack.push(leftString / rightString);
+                }
+            }
+            else{
+                myStack.push(stoi(tokens[i]));
+            }
+        }
+        return myStack.top();
     }
 };
